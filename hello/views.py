bd795cc5b42cc0c2f4480aba24e5665a2faba833
Dr. Paul Bader <paul_bader@MANP_DOM.int>
2017-12-22 08:59:16 +0100

    com

--- hello/views.py
+++ hello/views.py
@@ -7,8 +7,9 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-     times = int(os.environ.get('TIMES',3))
+    times = int(os.environ.get('TIMES',3))
     return HttpResponse('Hello! ' * times)
+
 
 
 
