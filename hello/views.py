40287546194137b961549f43d10605941c569be5
Dr. Paul Bader <paul_bader@MANP_DOM.int>
2017-12-22 10:19:55 +0100

    com

--- hello/views.py
+++ hello/views.py
@@ -1,16 +1,12 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
-import os
-
 from .models import Greeting
 
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES', 3))
     return HttpResponse('Hello! ' * times)
-
-
-
 
 def db(request):
 
@@ -20,5 +16,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
-
